93b2f5be215f6d58402bf7aaf600c384dbcf2d08
zhangdapeng520 <1156956636@qq.com>
2024-08-17 09:27:04 +0800

    新增：初步封装根据字典查询的方法

--- fastzdp_sqlmodel/__init__.py
+++ fastzdp_sqlmodel/__init__.py
@@ -1,7 +1,10 @@
 from .add import add
 from .engine import get_engine
 from .table import init_table, create_table
-from .get import get_by_dict, get_all
+from .get import (
+    get_by_dict, get_all, get_by_dict_or,
+    get_by_dict_and,
+)
 
 __all__ = [
     "add",
@@ -10,4 +13,6 @@ __all__ = [
     "create_table",
     "get_all",
     "get_by_dict",
+    "get_by_dict_or",
+    "get_by_dict_and",
 ]
--- fastzdp_sqlmodel/get.py
+++ fastzdp_sqlmodel/get.py
@@ -1,4 +1,4 @@
-from sqlmodel import Session, select
+from sqlmodel import Session, select, or_
 
 
 def get_by_dict(engine, model, query_dict: dict):
@@ -12,7 +12,72 @@ def get_by_dict(engine, model, query_dict: dict):
         query = select(model)
         if isinstance(query_dict, dict):
             for k, v in query_dict.items():
-                query = query.where(getattr(model, k) == v)
+                if hasattr(model, k):
+                    query = query.where(getattr(model, k) == v)
+        results = session.exec(query)
+        return results.all()
+
+
+def get_by_dict_or(engine, model, query_dict: dict):
+    """
+    根据查询字典指定的键值对查询数据，键值对之间的关系是or的关系
+    :param engine: 引擎对象
+    :param model: 模型类
+    :param query_dict: 查询字典 {age:{gt:3,lt:33}}
+    """
+    with Session(engine) as session:
+        query = select(model)
+        if isinstance(query_dict, dict):
+            for k, v in query_dict.items():
+                if hasattr(model, k):
+                    if isinstance(v, dict):
+                        # or_(Hero.age <= 35, Hero.age > 90)
+                        mk = getattr(model, k)
+                        conditions = []
+                        for kk, vv in v.items():
+                            if kk == "==":
+                                conditions.append(mk == vv)
+                            elif kk == ">":
+                                conditions.append(mk > vv)
+                            elif kk == "<":
+                                conditions.append(mk < vv)
+                            elif kk == ">=":
+                                conditions.append(mk >= vv)
+                            elif kk == "<=":
+                                conditions.append(mk <= vv)
+                        query = query.where(or_(*conditions))
+        results = session.exec(query)
+        return results.all()
+
+
+def get_by_dict_and(engine, model, query_dict: dict):
+    """
+    根据查询字典指定的键值对查询数据，键值对之间的关系是and的关系
+    :param engine: 引擎对象
+    :param model: 模型类
+    :param query_dict: 查询字典 {age:{gt:3,lt:33}}
+    """
+    with Session(engine) as session:
+        query = select(model)
+        if isinstance(query_dict, dict):
+            for k, v in query_dict.items():
+                if hasattr(model, k):
+                    if isinstance(v, dict):
+                        # or_(Hero.age <= 35, Hero.age > 90)
+                        mk = getattr(model, k)
+                        conditions = []
+                        for kk, vv in v.items():
+                            if kk == "==":
+                                conditions.append(mk == vv)
+                            elif kk == ">":
+                                conditions.append(mk > vv)
+                            elif kk == "<":
+                                conditions.append(mk < vv)
+                            elif kk == ">=":
+                                conditions.append(mk >= vv)
+                            elif kk == "<=":
+                                conditions.append(mk <= vv)
+                        query = query.where(*conditions)
         results = session.exec(query)
         return results.all()
 
