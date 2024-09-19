a321a1d842dfac0bbbbffe9b8dd5024e0c07f140
张大鹏 <1156956636@qq.com>
2024-09-19 08:29:16 +0800

    新增: 录播课代码

---
+++ examples/c03_teach_20240918/c07_find_all.py
@@ -0,0 +1,34 @@
+from tkinter.font import names
+
+import sqlmodel
+import typing
+
+
+class Hero(sqlmodel.SQLModel, table=True):
+    id: typing.Optional[int] = sqlmodel.Field(default=None, primary_key=True)
+    name: str
+    real_name: str
+    age: typing.Optional[int] = None
+
+
+db_url = "mysql+pymysql://root:zhangdapeng520@127.0.0.1:3306/fastzdp_sqlmodel?charset=utf8mb4"
+engine = sqlmodel.create_engine(db_url, echo=True)
+
+sqlmodel.SQLModel.metadata.drop_all(engine)
+sqlmodel.SQLModel.metadata.create_all(engine)
+
+# add
+with sqlmodel.Session(engine) as session:
+    zs1 = Hero(name="张三1", real_name="张三", age=33)
+    session.add(zs1)
+    zs2 = Hero(name="张三2", real_name="张三", age=33)
+    session.add(zs2)
+    zs3 = Hero(name="张三3", real_name="张三", age=33)
+    session.add(zs3)
+
+    # 查找
+    query = sqlmodel.select(Hero)
+    heros = session.exec(query)
+    print(list(heros))
+
+    session.commit()
