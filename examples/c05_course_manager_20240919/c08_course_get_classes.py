1bb097c64171f5c75e2843fac3b3e3a994b6e200
张大鹏 <1156956636@qq.com>
2024-09-19 11:06:43 +0800

    新增: 录播课代码

---
+++ examples/c05_course_manager_20240919/c08_course_get_classes.py
@@ -0,0 +1,52 @@
+import time
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+import fastzdp_sqlmodel as fasm
+
+
+class Classes(SQLModel, table=True):
+    """班级表"""
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+
+
+class Course(SQLModel, table=True):
+    """课程表"""
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str  # 课程名称
+    class_id: Optional[int] = None  # 班级 ID
+    class_name: Optional[str] = None  # 班级名称
+    order: Optional[int] = 0  # 序号
+    add_time: Optional[int] = None  # 添加时间
+    price: int  # 价格
+
+
+class CourseVideo(SQLModel, table=True):
+    """课程视频表"""
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str  # 视频名称
+    order: Optional[int] = 0  # 序号
+    add_time: Optional[int] = None  # 添加时间
+    course_id: Optional[int] = None  # 课程 ID
+    course_name: Optional[str] = None  # 课程名称
+
+
+engine = fasm.get_engine(password="zhangdapeng520", database="fastzdp_sqlmodel", echo=False)
+fasm.init_table(engine)
+
+fasm.add(engine, Classes(name="Python 零基础快速入门班"))
+fasm.add(engine, Classes(name="FastAPI+Vue3 全栈班"))
+
+c1 = {
+    "name": "FastAPI零基础入门",
+    "class_id": 2,
+    "class_name": "FastAPI+Vue3 全栈班",
+    "order": 1,
+    "price": 248,
+    "add_time": int(time.time())
+}
+fasm.add(engine, Course(**c1))
+
+data=fasm.get_by_dict(engine,Course,{"class_id":2})
+print(data)
