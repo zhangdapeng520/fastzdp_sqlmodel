98cdc23728b411cc1bda912150254d102458d89d
张大鹏 <1156956636@qq.com>
2024-09-24 20:42:08 +0800

    新增: get_sum支持等值查询

--- fastzdp_sqlmodel/get_analysis.py
+++ fastzdp_sqlmodel/get_analysis.py
@@ -1,18 +1,30 @@
 from sqlmodel import Session, select, func
 
 
-def get_sum(engine, model, column):
+def get_sum(
+        engine,
+        model,
+        column,
+        query_dict=None,
+):
     """
     获取求和结果
     :param engine 引擎对象
     :param model 模型对象
     :param column 要求和的列明
+    :param query_dict 等值查询条件, 比如 {class_id:14}
     """
     if not hasattr(model, column):
         return 0
     total = 0
     with Session(engine) as session:
         query = select(func.sum(getattr(model, column)))
+        # 等值查询
+        if isinstance(query_dict, dict):
+            for k,v in query_dict.items():
+                if hasattr(model,k):
+                    query = query.where(getattr(model,k)==v)
+        # 执行SQL
         result = session.exec(query).one()
         total = float(result) if result else 0
     return total
